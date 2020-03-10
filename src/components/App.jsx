0741263a3a199763796e1684859522c0cc5fd7b1
Bohomazov Dmitriy <bohomazov.dmitriy@gmail.com>
2020-03-10 22:09:32 +0200

    deleted films

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,38 @@
 import React from "react";
 import { moviesData } from "../moviesData";
+import MovieItem from "./MovieItem";
 
-console.log(moviesData);
 
 class App extends React.Component{
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       movies: moviesData
-    }
-
+    };
   }
+
+  removeMovie = movie => {
+    const updateMovies = this.state.movies.filter(function (item) {
+      return item.id !== movie.id;
+    });
+    console.log(updateMovies)
+    this.setState({
+          movies: updateMovies
+        }
+    )
+  }
+
   render() {
     console.log(this);
     return(
-        <div>{ this.state.movies.map((movie) => <p>{movie.title}</p> ) }</div>
+        <div>{ this.state.movies.map( movie => {
+            return <MovieItem
+                key={movie.id}
+                movie={movie}
+                removeMovie={this.removeMovie}
+            />
+        })}
+        </div>
     );
   }
 }
