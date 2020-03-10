b498b1874c56fd243457fc47eb9f71c9d8b567a3
Bohomazov Dmitriy <bohomazov.dmitriy@gmail.com>
2020-03-10 16:27:26 +0200

    render title

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { moviesData } from "../moviesData";
 
-function App() {
-  return <div>Hello ReactWarriors!</div>;
+console.log(moviesData);
+
+class App extends React.Component{
+  constructor() {
+    super();
+    this.state = {
+      movies: moviesData
+    }
+
+  }
+  render() {
+    console.log(this);
+    return(
+        <div>{ this.state.movies.map((movie) => <p>{movie.title}</p> ) }</div>
+    );
+  }
 }
+
+/*
+function App() {
+  return <div>{ moviesData[0].title }</div>;
+}
+*/
 
 export default App;
